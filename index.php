32f75565943a95746f66844499937c3db801ce4e
AmelieGosiau <amelie.gosiau@hotmail.com>
2019-08-17 00:26:02 +0200

    Update index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+<title>Doeda</title>
+</head>
+<body>
+<div class="header">
+<h1>Todo application</h1>
+</div>
+<form>
+<input type="text" name="task" class="task">
+</form>
+</body>
+</html>
